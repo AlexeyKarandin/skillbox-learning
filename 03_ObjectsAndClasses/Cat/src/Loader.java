438b0fc3b6c12cb48e6ef0cdbfaf2732e6949c56
Карандин Алексий <specnazdelta62@mail.ru>
2021-01-09 19:20:33 +0300

    class and object - cat experiments

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -1,10 +1,35 @@
 
-public class Loader
-{
-    public static void main(String[] args)
-    {
-        Cat cat = new Cat();
+public class Loader {
+    public static void main(String[] args) {
+        Cat murca = new Cat();
+        Cat barsic = new Cat();
+        int counter = 0;
+        while (!murca.getStatus().equals("Dead")) {
+            if (!murca.getStatus().equals("Dead"))
+                murca.meow();
+            if (murca.getStatus().equals("Dead")) {
+                System.out.println("Померла Мурка...");
+                System.out.println("Мяукнула мурка: " + counter + " раз");
+            }
+            counter++;
+            continue;
+        }
+        // пытался понять почему барсик взрывается сразу, потом понял, что мне не выводится, что он выпил. Решил создать счетчик выпитого
+        counter = 0;
 
-        System.out.println(cat.getStatus());
+        while (!(barsic.getStatus() == ("Exploded"))) {
+            barsic.drink(1.0);
+
+            if (barsic.getStatus().equals("Exploded")) {
+                System.out.println("Взорвался барсик...");
+                System.out.println("Барсик выпил: " + counter + " литров...");
+                if (counter > 7_000) {
+                    System.out.println("Проглот");
+                }
+            }
+            counter++;
+            continue;
+        }
+
     }
 }
