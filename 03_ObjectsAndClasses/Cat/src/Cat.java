d762e001ad17227929dc5551d4ef7be613af3a4d
Карандин Алексий <specnazdelta62@mail.ru>
2021-01-10 19:03:52 +0300

    добавил статичекую переменную для подсчета созданных кошек, так же при проверке статуса кошки удаляются если умерли или взорваны, а т.к. пока объекты не удаляются можно уйти в минус.

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -1,20 +1,27 @@
 
 public class Cat {
-    private double originWeight;
+    private final double originWeight;
     private double weight;
 
-    private double minWeight;
-    private double maxWeight;
+    private final double minWeight;
+    private final double maxWeight;
     private double feedCounter = .0;
+    private static int count;
 
     public Cat() {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
+        count++;
 
     }
 
+    // метод возвращаемый кол-во живых кошек, т.к. переменная count приватная,
+    // то получить данную переменную можно только через данный метод
+    public int getCount() {
+        return count;
+    }
     // подсчет съеденного для текущего экземпляра
     public double getFeedCounter() {
         return feedCounter;
@@ -22,23 +29,37 @@ public class Cat {
 
     // Метод для похода в туалет
     public void pee() {
-        --weight;
-        System.out.println("Ппссссс....");
+        if (weight > minWeight) {
+            --weight;
+            System.out.println("Ппссссс....");
+        } else
+            System.out.println("Кошку пора закопать...");
     }
 
     public void meow() {
-        weight = weight - 1;
-        System.out.println("Meow");
+        if (weight > minWeight) {
+            --weight;
+            System.out.println("Meow");
+        } else
+            System.out.println("Кошку пора закопать...");
     }
 
     public void feed(Double amount) {
-        weight = weight + amount;
-        // производим подсчет съеденной еды
-        feedCounter += amount;
+        if (weight < maxWeight) {
+            weight += amount;
+            // производим подсчет съеденной еды
+            feedCounter += amount;
+        } else
+            System.out.println("Кошку пора закопать...");
     }
 
     public void drink(Double amount) {
-        weight = weight + amount;
+        if (weight < maxWeight) {
+            weight += amount;
+            // производим подсчет съеденной еды
+            feedCounter += amount;
+        } else
+            System.out.println("Кошку пора закопать...");
     }
 
     public Double getWeight() {
@@ -47,8 +68,10 @@ public class Cat {
 
     public String getStatus() {
         if (weight < minWeight) {
+            count--;
             return "Dead";
         } else if (weight > maxWeight) {
+            count--;
             return "Exploded";
         } else if (weight > originWeight) {
             return "Sleeping";
--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -1,10 +1,13 @@
-
+/**
+ * Не совсем верно отрабатывает getCount т.к. циклы завязаны на вызове getStatus
+ */
 public class Loader {
     public static void main(String[] args) {
         Cat murca = new Cat();
         Cat barsic = new Cat();
         int counter = 0;
         barsic.pee();
+        System.out.println(murca.getCount());
         while (!murca.getStatus().equals("Dead")) {
             if (!murca.getStatus().equals("Dead"))
                 murca.meow();
@@ -16,17 +19,19 @@ public class Loader {
         }
         // пытался понять почему барсик взрывается сразу, потом понял, что мне не выводится, что он выпил. Решил создать счетчик выпитого
         counter = 0;
-
+        System.out.println(murca.getCount());
         while (!(barsic.getStatus().equals("Exploded"))) {
             barsic.drink(1.0);
             barsic.feed(2.0);
             if (barsic.getStatus().equals("Exploded")) {
                 System.out.println("Взорвался барсик...");
+                // добавил, что бы выдало, что пора закапывать
+                barsic.drink(1.0);
                 System.out.println("Барсик выпил: " + counter + " литров...");
                 System.out.println("И съел: " + barsic.getFeedCounter());
             }
             counter++;
         }
-
+        System.out.println(murca.getCount());
     }
 }
