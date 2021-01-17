46faee0de4dce699362dd5c6ac159510a07c6ede
Карандин Алексий <specnazdelta62@mail.ru>
2021-01-17 12:35:50 +0300

    Подключил внешнюю библиотеку airport-1.0.1.jar В консоль кол-во самолетов выводится.

---
+++ 03_ObjectsAndClasses/Experiments/src/Experiments.java
@@ -0,0 +1,23 @@
+import com.skillbox.airport.Aircraft;
+import com.skillbox.airport.Airport;
+
+public class Experiments {
+    public static void main(String[] args) {
+        Airport airport = Airport.getInstance();
+        int counter = 1;
+        for (Aircraft crafts :
+                airport.getAllAircrafts()) {
+            System.out.print(crafts + "\t");
+            if (counter % 10 == 0) {
+                System.out.println();
+                counter = 1;
+            }
+            counter++;
+        }
+        System.out.println();
+        System.out.println(airport.getTerminals());
+
+        int craftsSize = airport.getAllAircrafts().size();
+        System.out.println("Кол-во самолетов: " + craftsSize);
+    }
+}
