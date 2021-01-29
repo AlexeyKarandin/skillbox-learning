d48b782be152b5b265e0d00fe9bb40407b639e81
Карандин Алексий <specnazdelta62@mail.ru>
2021-01-30 02:42:10 +0300

    Тесты прошли успешно по TrucksContainersAndBoxes

--- 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -4,21 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String boxes = scanner.nextLine();
-        final int MAX_BOXES = Integer.parseInt(boxes);
-        final int MAX_CARGO = 12;
-        final int MAX_CONTAINER = 27;
-
+        int boxes = Integer.parseInt(scanner.nextLine());
+        final String LS = System.lineSeparator();
+        final int MAX_IN_CARGO = 12;
+        final int MAX_IN_CONTAINER = 27;
         int counterContainer = 1;
         int counterCargo = 1;
-        for (int i = 1; i <= Integer.parseInt(boxes); counterCargo++) {
-            System.out.println("Грузовик: " + counterCargo);
-            for (counterContainer = 1; counterContainer <= MAX_CONTAINER; counterContainer++) {
-                System.out.println("\tКонтейнер: " + counterContainer);
-
+        int sourceContainer = boxes % MAX_IN_CONTAINER == 0 ? boxes / MAX_IN_CONTAINER : (boxes / MAX_IN_CONTAINER) + 1;
+        int sourceCargo = sourceContainer % MAX_IN_CARGO == 0 ? sourceContainer / MAX_IN_CARGO : (sourceContainer / MAX_IN_CARGO) + 1;
+        for (int i = 1; i <= sourceCargo; i++) {
+            System.out.println("Грузовик: " + i);
+            for (int j = 1; j <= MAX_IN_CARGO && counterCargo <= sourceContainer; j++, counterCargo++) {
+                System.out.println("\tКонтейнер: " + counterCargo);
+                for (int k = 1; k <= MAX_IN_CONTAINER && counterContainer <= boxes; k++, counterContainer++) {
+                    System.out.println("\t\tЯщик: " + counterContainer);
+                }
             }
 
         }
+        System.out.printf("Необходимо:" + LS + "грузовиков - %d шт." + LS + "контейнеров - %d шт.", sourceCargo, sourceContainer);
 
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
