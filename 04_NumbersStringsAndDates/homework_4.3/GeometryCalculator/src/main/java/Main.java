26dc28edc062bfe2f64baa3588d6eaaebb873249
Alexey <karandinalexey@gmail.com>
2021-01-29 13:48:37 +0300

    Выполнил ДЗ 4.3 основное задание

--- 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(GeometryCalculator.getCircleSquare(20));
+        System.out.println(GeometryCalculator.getSphereVolume(20));
+        System.out.println(GeometryCalculator.isTrianglePossible(1., 2., 1.5));
+        System.out.println(GeometryCalculator.getTriangleSquare(1., 2., 1.5));
     }
 }
