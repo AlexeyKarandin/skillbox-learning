bb93849ad0a5391d14aa211bbe7e115bf9dba5b5
Alexey <karandinalexey@gmail.com>
2021-01-29 09:08:14 +0300

    Исправил вывод минимального значения Double

--- 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
@@ -20,7 +20,7 @@ public class Main {
     System.out.println(Float.MAX_VALUE);
     System.out.println(Float.MIN_VALUE);
     System.out.println(Double.MAX_VALUE);
-    System.out.println(Double.MIN_VALUE);
+    System.out.println(-Double.MIN_VALUE);
 
   }
 
