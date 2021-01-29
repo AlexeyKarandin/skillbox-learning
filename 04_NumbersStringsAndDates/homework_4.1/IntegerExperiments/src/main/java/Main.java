fb36be9f2cc5fcbf0f34d2946e5825d5e87b7cb4
Alexey <karandinalexey@gmail.com>
2021-01-29 09:08:14 +0300

    Исправил вывод минимального значения Double и Float

--- 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
@@ -18,9 +18,9 @@ public class Main {
     System.out.println(Long.MAX_VALUE);
     System.out.println(Long.MIN_VALUE);
     System.out.println(Float.MAX_VALUE);
-    System.out.println(Float.MIN_VALUE);
+    System.out.println(-Float.MIN_VALUE);
     System.out.println(Double.MAX_VALUE);
-    System.out.println(Double.MIN_VALUE);
+    System.out.println(-Double.MIN_VALUE);
 
   }
 
