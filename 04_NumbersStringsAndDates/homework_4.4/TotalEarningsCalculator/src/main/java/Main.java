d3cbbe66eb9e61d0cbb9ab6047dafc472653e1f1
Alexey <karandinalexey@gmail.com>
2021-02-08 16:10:00 +0300

    Выполнил ДЗ 4.4 TestEarningsCalculator(с substring())

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -5,20 +5,23 @@ public class Main {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
         //TODO: напишите ваш код, результат вывести в консоль
         String digits = "";
+        Integer first = null;
+        Integer second = null;
         int sum = 0;
         boolean isDigest = false;
         for (int i = 0; i < text.length(); i++) {
-            if (Character.isDigit(text.charAt(i))) {
-                isDigest = true;
-                digits += text.charAt(i);
-            } else {
-                if (isDigest) {
-                    sum += Integer.parseInt(digits);
-                    isDigest = false;
-                    digits = "";
-                }
+            isDigest =  Character.isDigit(text.charAt(i));
+            if (isDigest && first == null && second == null) {
+                first = i;
+            } else if (isDigest) {
+                second = i;
+            } else if (!isDigest && second != null && first != null) {
+                sum += Integer.parseInt(text.substring(first, ++second));
+                second = first = null;
             }
+
         }
+
         System.out.println(sum);
     }
 
