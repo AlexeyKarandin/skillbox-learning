43dd7f4676095d2798d2c1c024c13b9937c70491
Alexey <karandinalexey@gmail.com>
2021-02-08 15:10:27 +0300

    Выполнил ДЗ 4.4 TestEarningsCalculator

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,25 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        //TODO: напишите ваш код, результат вывести в консоль
+        String digits = "";
+        int sum = 0;
+        boolean isDigest = false;
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isDigit(text.charAt(i))) {
+                isDigest = true;
+                digits += text.charAt(i);
+            } else {
+                if (isDigest) {
+                    sum += Integer.parseInt(digits);
+                    isDigest = false;
+                    digits = "";
+                }
+            }
+        }
+        System.out.println(sum);
+    }
 
 }
