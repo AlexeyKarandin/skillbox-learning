e52f81fba18dc5956a03e3bc832cc640cef96974
Alexey <karandinalexey@gmail.com>
2021-02-15 12:55:22 +0300

    Все тесты задания searchAndReplaceDiamonds прошли успешно.

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -1,11 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912", "***");
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+        boolean isHaveMasking = false;
+        int frstIndex = 0;
+        int lastIndex = 0;
+        while (true) {
+            isHaveMasking = text.indexOf('<') != -1 ? true : false;
+            isHaveMasking = text.indexOf('>') != -1 ? true : false;
+            if (!isHaveMasking) {
+                break;
+            }
+            frstIndex = text.indexOf('<');
+            // +1 т.к. last не входит в диапазон
+            lastIndex = text.indexOf('>') + 1;
+            text = text.substring(0, frstIndex) + placeholder + text.substring(lastIndex);
+            System.out.println(text);
+        }
         return text;
     }
 
