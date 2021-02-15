97a1ba8fa453cbe2e0c9636757ac9bbd3c99714a
Alexey <karandinalexey@gmail.com>
2021-02-15 22:59:23 +0300

    Задание №1 из ДЗ 4.5. Тесты пройдены успешно.

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -5,19 +5,18 @@ public class Main {
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        boolean isHaveMasking = false;
-        int frstIndex = 0;
-        int lastIndex = 0;
+        boolean isHaveMasking;
+        int firstIndex;
+        int lastIndex;
         while (true) {
-            isHaveMasking = text.indexOf('<') != -1 ? true : false;
-            isHaveMasking = text.indexOf('>') != -1 ? true : false;
+            isHaveMasking = (text.indexOf('<') != -1) && (text.indexOf('>') != -1);
             if (!isHaveMasking) {
                 break;
             }
-            frstIndex = text.indexOf('<');
+            firstIndex = text.indexOf('<');
             // +1 т.к. last не входит в диапазон
             lastIndex = text.indexOf('>') + 1;
-            text = text.substring(0, frstIndex) + placeholder + text.substring(lastIndex);
+            text = text.substring(0, firstIndex) + placeholder + text.substring(lastIndex);
             System.out.println(text);
         }
         return text;
--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,21 @@
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        calculateSalarySum("Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей");
+    }
 
-  }
+    public static int calculateSalarySum(String text) {
+        //TODO: реализуйте метод
+        int sum = 0;
 
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
+        String[] digestArr = text.trim().split("[^0-9]");
+        for (String s : digestArr) {
+            if (s.isEmpty())
+                continue;
+            sum += Integer.parseInt(s);
+        }
+        return sum;
+    }
 
 }
