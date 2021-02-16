21f9e3238d0deeed19206411f00133d2005c41c0
Alexey <karandinalexey@gmail.com>
2021-02-16 10:51:25 +0300

    Задание №2 из ДЗ 4.5. Тесты пройдены успешно.

--- 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
@@ -1,12 +1,33 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        String text = "Everyone could notice that people often complain and say t.";
+        splitTextIntoWords(text);
+    }
 
-  }
-
-  public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
-  }
+    public static String splitTextIntoWords(String text) {
+        //TODO реализуйте метод
+        StringBuilder builder = new StringBuilder();
+        String[] resultText = text.split("[^\\’||\\w]+[0-9]*\\W?");
+        if (resultText[0].equals(""))
+            return "";
+        for (int i = 0; i < resultText.length; i++) {
+            // долго думал почему не проходит текст, оказалось, что в конец добавляется пустая строка
+            if (i == resultText.length - 1) {
+                builder.append(resultText[i]);
+                continue;
+            }
+            builder.append(resultText[i] + "\r\n");
+        }
+        return builder.toString();
+    }
 
 }
