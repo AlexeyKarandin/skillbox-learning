514bb801d72d85677ca5dddd9762e7679c78bc75
Alexey <karandinalexey@gmail.com>
2021-02-17 22:50:20 +0300

    Задание №5 из ДЗ 4.5. Тесты пройдены успешно.

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -1,11 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(searchAndReplaceDiamonds("Номер кредитной карты <4008> 1234 <5678> 8912", "***"));
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+        text = text.replaceAll("<[\\d\\s]+>", placeholder);
         return text;
     }
 
