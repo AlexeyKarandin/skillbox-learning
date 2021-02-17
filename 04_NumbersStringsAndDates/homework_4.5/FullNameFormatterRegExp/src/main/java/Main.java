067beb720a0988c07286138ad28be634975d3a03
Alexey <karandinalexey@gmail.com>
2021-02-17 13:56:49 +0300

    Задание №3 из ДЗ 4.5. Тесты пройдены успешно.

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,18 +1,29 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            //TODO:напишите ваш код тут, результат вывести в консоль.
+            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+            input = input.trim();
+            Pattern pattern = Pattern.compile("[А-я-?]+ [А-я]+ [А-я]+");
+            if (pattern.matcher(input).matches()) {
+                String[] fio = input.split("\\s");
+                System.out.printf("Фамилия: %s%nИмя: %s%nОтчество: %s", fio[0], fio[1], fio[2]);
+            } else {
+                System.out.println("Введенная строка не является ФИО");
+            }
+
+        }
     }
-  }
 
 }
