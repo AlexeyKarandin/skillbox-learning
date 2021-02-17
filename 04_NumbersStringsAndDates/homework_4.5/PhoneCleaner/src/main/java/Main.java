7ba6183e8930facf198ea3db7138fc6f03ea3262
Alexey <karandinalexey@gmail.com>
2021-02-17 22:35:14 +0300

    Задание №4 из ДЗ 4.5. Тесты пройдены успешно.

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -1,17 +1,36 @@
+import java.io.BufferedReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder builder = new StringBuilder();
+        Pattern pattern1 = Pattern.compile("7(\\d{10})|$");
+        Pattern pattern2 = Pattern.compile("8(\\d{10})|$");
+        Pattern pattern3 = Pattern.compile("9(\\d{9})|$");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            //TODO:напишите ваш код тут, результат вывести в консоль.
+            input = input.trim();
+            input = input.trim().replaceAll("\\D+", "");
+
+            if (pattern1.matcher(input).matches()) {
+                System.out.println(input);
+            } else if (pattern2.matcher(input).matches()) {
+                System.out.println(input.replaceFirst("8", "7"));
+            } else if (pattern3.matcher(input).matches()) {
+                System.out.println("7" + input);
+            } else {
+                System.out.println("Неверный формат номера\n");
+            }
+        }
     }
-  }
 
 }
