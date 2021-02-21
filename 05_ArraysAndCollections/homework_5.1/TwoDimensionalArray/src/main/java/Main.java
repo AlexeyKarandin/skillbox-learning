2a14327ffa5c290497bf02d47bb6509edc1b5c50
Alexey <karandinalexey@gmail.com>
2021-02-21 18:51:18 +0300

    Дополнительное задание из ДЗ 5.1. Цикл вывода массива заменил на foreach

--- 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         //Распечатайте сгенерированный в классе TwoDimensionalArray.java двумерный массив
         char[][] mass = TwoDimensionalArray.getTwoDimensionalArray(10);
-        for (int i = 0; i < mass.length; i++) {
+        for (char[] chars : mass) {
             for (int j = 0; j < mass.length; j++) {
-                System.out.print(mass[i][j]);
+                System.out.print(chars[j]);
             }
-            System.out.print(System.lineSeparator());;
+            System.out.print(System.lineSeparator());
         }
     }
 }
