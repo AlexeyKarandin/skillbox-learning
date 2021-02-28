f828ee5077567762f0db0d9e4bea70d10c1d9c72
Alexey Karandin <a.karandin@isimplelab.com>
2021-02-28 12:46:23 +0300

    Задание №1 из ДЗ 5.3. Тесты успешны.

--- 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/EmailList.java
+++ 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,26 @@
-import java.util.Collections;
+import org.apache.commons.collections.list.TreeList;
+
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
 
 public class EmailList {
 
+    private final Set<String> mailSet = new TreeSet<>();
+    private final List<String> mailList = new TreeList();
+    Pattern addMail = Pattern.compile("\\w+@\\w+\\.[A-z]{2,3}");
+
     public void add(String email) {
         // TODO: валидный формат email добавляется
+        if (addMail.matcher(email).matches()) {
+            mailSet.add(email.toLowerCase());
+        }
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
+        mailList.addAll(mailSet);
+        return mailList;
     }
-
 }
--- 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/Main.java
@@ -1,33 +1,30 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
-    /* TODO:
-        Пример вывода списка Email, после ввода команды LIST в консоль:
-        test@test.com
-        hello@mail.ru
-        - каждый адрес с новой строки
-        - список должен быть отсортирован по алфавиту
-        - email в разных регистрах считается одинаковыми
-           hello@skillbox.ru == HeLLO@SKILLbox.RU
-        - вывод на печать должен быть в нижнем регистре
-           hello@skillbox.ru
-        Пример вывода сообщения об ошибке при неверном формате Email:
-        "Неверный формат email"
-    */
 
     public static void main(String[] args) {
+        EmailList emailList = new EmailList();
         Scanner scanner = new Scanner(System.in);
-        
+        final String enterList = "LIST";
+        Pattern addMail = Pattern.compile("ADD .+");
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("0")) {
+            if (input.equals("exit")) {
                 break;
+            } else if (input.trim().equals(enterList)) {
+                for (String str :
+                        emailList.getSortedEmails()) {
+                    System.out.println(str);
+                }
+            } else if (addMail.matcher(input).matches()) {
+                emailList.add(input.replaceAll("ADD ", ""));
+            } else {
+                System.out.println(WRONG_EMAIL_ANSWER);
             }
-            
-            //TODO: write code here
-            
+
         }
+
     }
 }
