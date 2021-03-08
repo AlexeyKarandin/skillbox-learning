b1a502da66e16354a5d0206a1bcfd6a54ab8473b
Alexey Karandin <a.karandin@isimplelab.com>
2021-03-09 00:18:50 +0300

    Задание №1 из ДЗ 5.5. Тесты успешны. Судя по времени входа в методы и выход, самый долгий это бинарный поиск(предполагаю, что это из-за учета времени сортировки).

--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,29 +1,91 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class CoolNumbers {
 
+    public static StringBuilder builder = new StringBuilder();
+
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        List<String> list = new LinkedList<>();
+
+        for (int i = 0; i < 2_000_010; i++)
+            list.add(getNumber());
+        return list;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
+        long startSearch = System.currentTimeMillis();
+        for (String s : list) {
+            if (s.equals(number)) {
+                // не переводил в наносекунды, т.к. получаются большие цифры, для перевода можно воспользоваться строкой ниже
+                time = TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis() - startSearch);
+                //time = System.currentTimeMillis() - startSearch;
+                return true;
+            }
+        }
+        time = TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis() - startSearch);
         return false;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        long startSearch = System.currentTimeMillis();
+        Collections.sort(sortedList);
+        if (Collections.binarySearch(sortedList, number) >= 0) {
+            time = TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis() - startSearch);
+            //time = System.currentTimeMillis() - startSearch;
+            return true;
+        } else {
+            time = TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis() - startSearch);
+            return false;
+        }
+
     }
 
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        long startSearch = System.currentTimeMillis();
+        if (hashSet.contains(number)) {
+            time = TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis() - startSearch);
+            //time = System.currentTimeMillis() - startSearch;
+            return true;
+        } else {
+            time = TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis() - startSearch);
+            return false;
+        }
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        long startSearch = System.currentTimeMillis();
+        if (treeSet.contains(number)) {
+            time = TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis() - startSearch);
+            return true;
+        } else {
+            time = TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis() - startSearch);
+            return false;
+        }
     }
 
+    public static String getRand(int min, int max) {
+        int i;
+        i = (int) (Math.random() * max) + min;
+        return i < 10 ? "0" + i : String.valueOf(i);
+    }
+
+    public static String getNumber() {
+        final int maxR = 199;
+        final int minR = 1;
+        builder = new StringBuilder();
+        final String LETTERS = "АВЕКМНОРСТУХ";
+        builder.append(LETTERS.charAt((int) (Math.random() * (LETTERS.length() - 1))));
+        builder.append((int) (Math.random() * 10));
+        builder.append((int) (Math.random() * 10));
+        builder.append((int) (Math.random() * 10));
+        builder.append(LETTERS.charAt((int) (Math.random() * (LETTERS.length() - 1))));
+        builder.append(LETTERS.charAt((int) (Math.random() * (LETTERS.length() - 1))));
+        builder.append(getRand(minR, maxR));
+        return builder.toString();
+    }
+
+    public static long time = 0;
 }
+
--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+
 public class Main {
     /*
     TODO:
@@ -8,5 +12,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        List<String> list;
+        list = CoolNumbers.generateCoolNumbers();
+        System.out.printf("Поиск перебором: М000ВВ01 %s, поиск занял %d\n",
+                CoolNumbers.bruteForceSearchInList(list, list.get((int) (Math.random() * 2000000))) ? "найден" : "не найден", CoolNumbers.time);
+        System.out.printf("Бинарный поиск: М000ВВ01 %s, поиск занял %d\n",
+                CoolNumbers.binarySearchInList(list, list.get((int) (Math.random() * 2000000))) ? "найден" : "не найден", CoolNumbers.time);
+        System.out.printf("Поиск в HashSet: М000ВВ01 %s, поиск занял %d\n",
+                CoolNumbers.searchInHashSet(new HashSet<>(list), list.get((int) (Math.random() * 2000000))) ? "найден" : "не найден", CoolNumbers.time);
+        System.out.printf("Поиск в TreeSet: М000ВВ01 %s, поиск занял %d\n",
+                CoolNumbers.searchInTreeSet(new TreeSet<>(list), list.get((int) (Math.random() * 2000000))) ? "найден" : "не найден", CoolNumbers.time);
     }
 }
