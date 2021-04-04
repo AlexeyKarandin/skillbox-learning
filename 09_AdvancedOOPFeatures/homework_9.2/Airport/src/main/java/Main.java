071436c89ae861e8baf94504f53ad967875f14ae
Карандин Алексий <specnazdelta62@mail.ru>
2021-04-04 23:59:33 +0300

    ДЗ 9.2 задание №2. Тест успешен.

--- 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
@@ -1,17 +1,29 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+
+import java.util.Calendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        Airport airport = Airport.getInstance();
 
+        System.out.println(findPlanesLeavingInTheNextTwoHours(airport));
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        Calendar calendar = Calendar.getInstance();
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.add(Calendar.HOUR_OF_DAY, 2);
+        return airport.getTerminals().stream()
+                .flatMap(terminal -> terminal.getFlights().stream())
+                .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE))
+                .filter(flight -> {
+                    calendar.setTime(flight.getDate());
+                    return calendar.before(calendar1);
+                }).collect(Collectors.toList());
     }
 
 }
