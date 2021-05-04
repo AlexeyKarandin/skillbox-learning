5dcb75ce0f94d63f878564d2b3368260afdde227
Карандин Алексий <specnazdelta62@mail.ru>
2021-05-04 21:51:27 +0300

    ДЗ 10.2 задание №2. Временное изменение

--- 10_ExceptionsDebuggingAndTesting/homework_10.2/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ 10_ExceptionsDebuggingAndTesting/homework_10.2/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -1,18 +1,17 @@
 import core.Line;
 import core.Station;
-import junit.framework.TestCase;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RouteCalculatorTest extends TestCase {
+public class RouteCalculatorTest {
 
     List<Station> route;
     List<Station> stations;
-    List<Station> actual;
     StationIndex stationIndex;
     RouteCalculator routeCalculator;
 
@@ -25,6 +24,13 @@ public class RouteCalculatorTest extends TestCase {
         Line line1 = new Line(1, "Кировско-Выборгская");
         Line line2 = new Line(2, "Московско-Петроградская");
         Line line3 = new Line(3, "Невско-Василеостровская");
+
+        route.add(new Station("Лесная", line1));
+        route.add(new Station("Автово", line1));
+        route.add(new Station("Парнас", line2));
+        route.add(new Station("Пионерская", line2));
+
+
         Station station1 = new Station("Владимирская", line1);
         Station station2 = new Station("Невский проспект", line2);
         Station station3 = new Station("Гостиный двор", line3);
@@ -54,25 +60,22 @@ public class RouteCalculatorTest extends TestCase {
 
     @After
     public void tearDown() {
-//        route = new ArrayList<>();
-//        stations = new ArrayList<>();
-//        stationIndex = new StationIndex();
     }
 
     @Test
     public void testGetShortestRoute() {
 
-      actual = routeCalculator.getShortestRoute(stationIndex.getStation("Лесная"),
-              stationIndex.getStation("Яблочная"));
-      assertEquals("123", actual.toString());
+        List<Station> actual = routeCalculator.getShortestRoute(stationIndex.getStation("Лесная"),
+                stationIndex.getStation("Яблочная"));
+        Assert.assertEquals("123", actual.toString());
 
     }
 
     @Test
     public void testCalculateDuration() {
         double actual = RouteCalculator.calculateDuration(route);
-        double expected = 14.5;
-        assertEquals("Маршрут: ", expected, actual, 0);
+        double expected = 8.5;
+        Assert.assertEquals(expected, actual, 0);
 
     }
 }
