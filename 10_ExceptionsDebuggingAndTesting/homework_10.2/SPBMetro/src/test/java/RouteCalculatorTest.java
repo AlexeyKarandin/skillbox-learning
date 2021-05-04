45756ea4d5c3130a1af05b75b4838f51a6227357
Alexey Karandin <a.karandin@isimplelab.com>
2021-05-04 23:05:56 +0300

    ДЗ 10.2 Для RouteCalculator() классы и методы покрыты на 100 %, строки на 92%.

--- 10_ExceptionsDebuggingAndTesting/homework_10.2/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ 10_ExceptionsDebuggingAndTesting/homework_10.2/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -24,38 +24,36 @@ public class RouteCalculatorTest {
         Line line1 = new Line(1, "Кировско-Выборгская");
         Line line2 = new Line(2, "Московско-Петроградская");
         Line line3 = new Line(3, "Невско-Василеостровская");
+        stationIndex.addLine(line1);
+        stationIndex.addLine(line2);
+        stationIndex.addLine(line3);
 
         route.add(new Station("Лесная", line1));
         route.add(new Station("Автово", line1));
         route.add(new Station("Парнас", line2));
         route.add(new Station("Пионерская", line2));
 
-
-        Station station1 = new Station("Владимирская", line1);
-        Station station2 = new Station("Невский проспект", line2);
-        Station station3 = new Station("Гостиный двор", line3);
         line1.addStation(new Station("Лесная", line1));
+        line1.addStation(new Station("Нарвская", line1));
         line1.addStation(new Station("Автово", line1));
+        line1.getStations().forEach(station -> stationIndex.addStation(station));
         line2.addStation(new Station("Парнас", line2));
+        line2.addStation(new Station("Озерки", line2));
         line2.addStation(new Station("Пионерская", line2));
-        line3.addStation(new Station("Морковная", line3));
-        line3.addStation(new Station("Яблочная", line3));
-
-        stations.add(station1);
-        stations.add(station2);
-        stations.add(station3);
-
-        stationIndex.addStation(station1);
-        stationIndex.addStation(station2);
-        stationIndex.addStation(station3);
-        stationIndex.addLine(line1);
-        stationIndex.addLine(line2);
-        stationIndex.addLine(line3);
+        line2.getStations().forEach(station -> stationIndex.addStation(station));
+        line3.addStation(new Station("Беговая", line3));
+        line3.addStation(new Station("Маяковская", line3));
+        line3.addStation(new Station("Рыбацкое", line3));
+        line3.getStations().forEach(station -> stationIndex.addStation(station));
+        stations.add(stationIndex.getStation("Автово"));
+        stations.add(stationIndex.getStation("Парнас"));
+        stationIndex.addConnection(stations);
+        stations.clear();
+        stations.add(stationIndex.getStation("Беговая"));
+        stations.add(stationIndex.getStation("Пионерская"));
         stationIndex.addConnection(stations);
 
         routeCalculator = new RouteCalculator(stationIndex);
-
-
     }
 
     @After
@@ -66,8 +64,19 @@ public class RouteCalculatorTest {
     public void testGetShortestRoute() {
 
         List<Station> actual = routeCalculator.getShortestRoute(stationIndex.getStation("Лесная"),
-                stationIndex.getStation("Яблочная"));
-        Assert.assertEquals("123", actual.toString());
+                stationIndex.getStation("Автово"));
+        Assert.assertEquals("[Лесная, Нарвская, Автово]",
+                actual.toString());
+        actual.clear();
+        actual = routeCalculator.getShortestRoute(stationIndex.getStation("Нарвская"),
+                stationIndex.getStation("Озерки"));
+        Assert.assertEquals("[Нарвская, Автово, Парнас, Озерки]",
+                actual.toString());
+        actual.clear();
+        actual = routeCalculator.getShortestRoute(stationIndex.getStation("Лесная"),
+                stationIndex.getStation("Рыбацкое"));
+        Assert.assertEquals("[Лесная, Нарвская, Автово, Парнас, Озерки, Пионерская, Беговая, Маяковская, Рыбацкое]",
+                actual.toString());
 
     }
 
@@ -76,6 +85,5 @@ public class RouteCalculatorTest {
         double actual = RouteCalculator.calculateDuration(route);
         double expected = 8.5;
         Assert.assertEquals(expected, actual, 0);
-
     }
 }
