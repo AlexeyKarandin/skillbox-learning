38466029ae4c7cef45d05cf6dab786ffc96a4482
Alexey Karandin <a.karandin@isimplelab.com>
2021-05-10 00:35:29 +0300

    ДЗ 11.1 Тесты успешны.

--- 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
@@ -1,6 +1,14 @@
+import java.io.File;
+import java.util.Arrays;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long sumSize;
+        File file = new File(path);
+        sumSize = Arrays.stream(file.listFiles()).mapToLong(file1 ->
+                file1.isDirectory() ? calculateFolderSize(file1.getAbsolutePath())
+                : file1.length()).sum();
+        return sumSize;
     }
 }
