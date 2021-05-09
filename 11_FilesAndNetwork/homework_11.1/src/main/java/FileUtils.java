e65560d8ef83110f0f58a7ff2d7cf942689d89d3
Alexey Karandin <a.karandin@isimplelab.com>
2021-05-10 02:23:02 +0300

    ДЗ 11.1 Тесты успешны. Добавил выводы ошибок для информативности.

--- 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
@@ -1,14 +1,31 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class FileUtils {
+    private static final Logger logger = LogManager.getLogger();
 
     public static long calculateFolderSize(String path) {
         long sumSize;
         File file = new File(path);
-        sumSize = Arrays.stream(file.listFiles()).mapToLong(file1 ->
-                file1.isDirectory() ? calculateFolderSize(file1.getAbsolutePath())
-                : file1.length()).sum();
-        return sumSize;
+        try {
+            if (!file.exists()) {
+                logger.error("Не найден путь: " + path);
+                throw new IOException();
+            }
+            sumSize = Arrays.stream(file.listFiles()).mapToLong(file1 -> {
+                logger.info("В обработке: " + file1.getAbsolutePath());
+                return file1.isDirectory() ? calculateFolderSize(file1.getAbsolutePath())
+                        : file1.length();
+            }).sum();
+            logger.info("Сумма составляет: " + sumSize + " byte");
+            return sumSize;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
 }
--- 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -1,5 +1,44 @@
+import java.security.InvalidParameterException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long sum;
+        String path;
+        while (true) {
+            System.out.print("Введите путь до папки(для выхода ввести exit): ");
+            path = scanner.nextLine();
+            if (path.equalsIgnoreCase("exit")) {
+                break;
+            }
+            try {
+                sum = FileUtils.calculateFolderSize(path);
+                if (sum == -1) throw new InvalidParameterException();
+                System.out.printf("Размер папки %s составляет: %s%n", path, getVieSum(sum));
+            } catch (InvalidParameterException e) {
+                System.out.println("Не указана или указана не верно директория поиска");
+            }
 
+        }
+    }
+
+    public static String getVieSum(Long sum) {
+        double viewSum;
+        if (sum < 1024) {
+            return (sum + " byte");
+        } else if (sum < 1_048_576) {
+            viewSum = (double)sum / 1024;
+            return (viewSum + " Kb");
+        } else if (sum < 1_073_741_824) {
+            viewSum = (double)sum / 1024;
+            viewSum /= 1024;
+            return (viewSum + " Mb");
+        } else {
+            viewSum = (double)sum / 1024;
+            viewSum /= 1024;
+            viewSum /= 1024;
+            return (viewSum + " Gb");
+        }
     }
 }
