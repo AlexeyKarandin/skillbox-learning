6547aacb05743f1092f19a58b77492c47122fe26
Alexey Karandin <a.karandin@isimplelab.com>
2021-05-12 15:17:42 +0300

    ДЗ 11.2 Тесты успешны. Ввод/вывод для работы в консоли допишу позже.

--- 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,34 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
 public class FileUtils {
+    private final static Logger logger = LogManager.getLogger();
+
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
         // TODO: write code copy content of sourceDirectory to destinationDirectory
+        Path pathIn = Paths.get(sourceDirectory);
+        Path pathOut = Paths.get(destinationDirectory);
+        try (Stream<Path> stream = Files.walk(pathIn)) {
+            if (!Files.exists(pathIn)) {
+                logger.error("Не найден путь: " + pathIn);
+                throw new IOException();
+            }
+            stream.forEach(path -> {
+                try {
+                    Files.copy(path, pathOut.resolve(pathIn.relativize(path)), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
