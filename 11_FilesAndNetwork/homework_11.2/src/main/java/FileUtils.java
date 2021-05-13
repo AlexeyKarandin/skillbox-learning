1601bfafe4628801a0a0d6b8b697a527f08e6f7c
Alexey Karandin <a.karandin@isimplelab.com>
2021-05-13 09:42:03 +0300

    ДЗ 11.2 Добавил реализацию через walkFileTree().

--- 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -2,10 +2,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.stream.Stream;
 
 public class FileUtils {
@@ -20,17 +18,54 @@ public class FileUtils {
                 logger.error("Не найден путь: " + pathIn);
                 throw new IOException();
             }
-            stream.forEach(path -> {
-                try {
-                    logger.info(String.format("Копируемый файл: %s. Путь файла: %s", path.getFileName(), path));
-                    Files.copy(path, pathOut.resolve(pathIn.relativize(path)), StandardCopyOption.REPLACE_EXISTING);
-                    logger.info(String.format("Файл %s скопирован в: %s", path.getFileName().toString(), pathOut.resolve(pathIn.relativize(path).toString())));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
+//            stream.forEach(path -> {
+//                try {
+//                    logger.info(String.format("Копируемый файл: %s. Путь файла: %s", path.getFileName(), path));
+//                    Files.copy(path, pathOut.resolve(pathIn.relativize(path)), StandardCopyOption.REPLACE_EXISTING);
+//                    logger.info(String.format("Файл %s скопирован в: %s", path.getFileName().toString(), pathOut.resolve(pathIn.relativize(path).toString())));
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+            Files.walkFileTree(pathIn, new MyVisitor(pathIn, pathOut));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    static class MyVisitor implements FileVisitor<Path> {
+
+        private final Path source;
+        private final Path target;
+
+        MyVisitor(Path source, Path target) {
+            this.source = source;
+            this.target = target;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            Path newDir = target.resolve(source.relativize(dir));
+            Files.copy(dir, newDir, StandardCopyOption.REPLACE_EXISTING);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Files.copy(file, target.resolve(source.relativize(file)), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) {
+            exc.printStackTrace();
+            return FileVisitResult.TERMINATE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+            return FileVisitResult.CONTINUE;
+        }
+    }
+
 }
