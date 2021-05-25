84e70f5fd9e2fbf993bd6297b05030c8fced0d16
Alexey Karandin <a.karandin@isimplelab.com>
2021-05-25 22:03:22 +0300

    ДЗ 11.4 Реализовал получение и загрузку изображений с сайта "https://lenta.ru/".

---
+++ 11_FilesAndNetwork/homework_11.4/src/main/java/Main.java
@@ -0,0 +1,29 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "https://lenta.ru/";
+        Document document;
+        try {
+            document = Jsoup.connect(url).get();
+            System.out.println(document.title());
+            Elements images = document.getElementsByClass("g-picture");
+            for (Element image : images) {
+                ImageIO.write(ImageIO.read(new URL(image.attr("abs:src"))), "jpg",
+                        new File("images/" + image.attr("src").substring(image.attr("src").lastIndexOf("/") + 1)));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
